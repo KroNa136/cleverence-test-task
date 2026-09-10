fce74ee39d7d2c8b5083061f2d99a0dc2475895e
KroNa136 <alina.tsybina.2002@yandex.ru>
2026-09-10 18:05:00 +0300

    Task 1 done

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using Task1;
+
+// Valid format (compression + decompression)
+
+string originalString = "aaabbcccdde";
+string compressedString = StringCompressor.Compress(originalString);
+string decompressedString = StringCompressor.Decompress(compressedString);
+
+Console.WriteLine($"Original string:\n{originalString}\n\nCompressed string:\n{compressedString}\n\nDecompressed string:\n{decompressedString}\n");
+Console.WriteLine("---------------------------------------------\n");
+
+// Invalid format (compression + decompression)
+
+string invalidFormatString = "q0w1e0r1t0y1";
+
+try
+{
+    string compressedInvalidFormatString = StringCompressor.Compress(invalidFormatString);
+}
+catch (Exception ex)
+{
+    OutputCompressingError(ex, invalidFormatString);
+}
+
+try
+{
+    string decompressedInvalidStringFormat = StringCompressor.Decompress(invalidFormatString);
+}
+catch (Exception ex)
+{
+    OutputDecompressingError(ex, invalidFormatString);
+}
+
+Console.WriteLine("---------------------------------------------\n");
+
+// Integer overflow when decompressing a string
+
+string integerOverflowString = "a999999999999999999";
+
+try
+{
+    string decompressedIntegerOverflowString = StringCompressor.Decompress(integerOverflowString);
+}
+catch (Exception ex)
+{
+    OutputDecompressingError(ex, integerOverflowString);
+}
+
+static void OutputCompressingError(Exception ex, string processedString)
+{
+    Console.WriteLine($"Got an exception when trying to compress the following string: {processedString}\n{ex.GetType()}: {ex.Message}\n");
+}
+
+static void OutputDecompressingError(Exception ex, string processedString)
+{
+    Console.WriteLine($"Got an exception when trying to decompress the following string: {processedString}\n{ex.GetType()}: {ex.Message}\n");
+}
---
+++ Task1/Task1/StringCompressor.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task1;
+
+/// <summary>
+/// A compressor for strings containing only lowercase letters.
+/// </summary>
+public static class StringCompressor
+{
+    /// <summary>
+    /// Compresses a string by replacing consecutive duplicate characters with a single character followed by the count of duplicates.
+    /// </summary>
+    /// <param name="str">The string to be compressed, which must contain only lowercase letters.</param>
+    /// <returns>The compressed string.</returns>
+    /// <exception cref="FormatException">Thrown when the provided string contains characters other than lowercase letters.</exception>
+    public static string Compress(string str)
+    {
+        if (string.IsNullOrEmpty(str) || str.Length == 1)
+            return str;
+
+        if (!MatchesDecompressedFormat(str))
+            throw new FormatException("The provided string must contain only lowercase letters.");
+
+        StringBuilder sb = new();
+
+        char currentChar = str[0];
+        int currentCharCount = 1;
+
+        void Append()
+        {
+            sb.Append(currentChar);
+
+            if (currentCharCount > 1)
+                sb.Append(currentCharCount);
+        }
+
+        for (int i = 1; i < str.Length; i++)
+        {
+            char nextChar = str[i];
+
+            if (nextChar.Equals(currentChar))
+            {
+                currentCharCount++;
+            }
+            else
+            {
+                Append();
+
+                currentChar = nextChar;
+                currentCharCount = 1;
+            }
+        }
+
+        Append();
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// Decompresses a string from a compressed format where characters are followed by their repeat counts.
+    /// </summary>
+    /// <param name="str">The compressed string to decompress.</param>
+    /// <returns>The original, decompressed string.</returns>
+    /// <exception cref="FormatException">Thrown when the input string does not match the expected compressed format.</exception>
+    /// <exception cref="OverflowException">Thrown when one of the counts inside the compressed string exceeds the maximum integer value.</exception>
+    public static string Decompress(string str)
+    {
+        if (string.IsNullOrEmpty(str) || str.Length == 1)
+            return str;
+
+        if (!MatchesCompressedFormat(str))
+            throw new FormatException("The provided string must have a valid compressed format (e.g. ab2c3d4).");
+
+        StringBuilder sb = new();
+
+        char currentChar = str[0];
+        StringBuilder currentCharCountSb = new();
+
+        void Append() => sb.Append(currentChar, repeatCount: (currentCharCountSb.Length > 0) ? int.Parse(currentCharCountSb.ToString()) : 1);
+
+        for (int i = 1; i < str.Length; i++)
+        {
+            char nextChar = str[i];
+
+            if (char.IsDigit(nextChar))
+            {
+                currentCharCountSb.Append(nextChar);
+            }
+            else
+            {
+                Append();
+
+                currentChar = nextChar;
+                currentCharCountSb.Clear();
+            }
+        }
+
+        Append();
+
+        return sb.ToString();
+    }
+
+    private static bool MatchesDecompressedFormat(string str)
+        => str.All(char.IsLower);
+
+    private static bool MatchesCompressedFormat(string str)
+    {
+        if (!str.All(ch => char.IsLower(ch) || char.IsDigit(ch)))
+            return false;
+
+        if (str.Length < 1 || !char.IsLower(str[0]))
+            return false;
+
+        char currentChar = str[0];
+
+        for (int i = 1; i < str.Length; i++)
+        {
+            char nextChar = str[i];
+
+            if (char.IsLetter(currentChar) && (nextChar.Equals('0') || nextChar.Equals('1')))
+                return false;
+        }
+
+        return true;
+    }
+}
